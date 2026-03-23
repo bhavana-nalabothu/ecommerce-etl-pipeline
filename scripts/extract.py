a7e94ef851e51d17aaf0265fd7101063a5b24eb1
Bhavana <bhavananalabothu1@gmail.com>
2026-03-23 01:12:48 -0500

    Added extract scripts from s3

---
+++ scripts/extract.py
@@ -0,0 +1,23 @@
+import boto3
+import pandas as pd
+
+def extract_from_s3(bucket_name, file_key):
+    # Create S3 client
+    s3 = boto3.client('s3')
+
+    # Download CSV from S3
+    response = s3.get_object(Bucket=bucket_name, Key=file_key)
+
+    # Read into pandas dataframe
+    df = pd.read_csv(response['Body'])
+
+    print("Data extracted successfully!")
+    print(df.head())
+
+    return df
+
+if __name__ == "__main__":
+    bucket = "ecommerce-etl-bucket-bhavana-303575244299-us-east-2-an"       
+    file_key = "Chocolate Sales.csv"       
+
+    extract_from_s3(bucket, file_key)
